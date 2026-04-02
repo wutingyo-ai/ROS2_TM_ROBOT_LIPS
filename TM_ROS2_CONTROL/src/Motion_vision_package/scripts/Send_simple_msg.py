dfe514e645818011f2a417f9a4ea692402f06e4b
wutingyo-ai <wutingyo@gmail.com>
2026-04-02 11:18:01 +0800

    cmake and simple msg

--- TM_ROS2_CONTROL/src/Motion_vision_package/scripts/Send_simple_msg.py
+++ TM_ROS2_CONTROL/src/Motion_vision_package/scripts/Send_simple_msg.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 
@@ -8,14 +9,17 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'user_input', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg.data = input("Enter a message to publish (or 'exit' to quit): ")
+        if msg.data == 'exit':
+            self.get_logger().info('Exiting...')
+            rclpy.shutdown()
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
